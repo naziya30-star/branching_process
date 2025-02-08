9eb819c6ca191224292234f21b6a82b03cb42132
Nazzz <naziya8142@gmail.com>
2025-02-08 21:27:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ def factorial(n):
     if n == 0 or n == 1:
         return 1
     else:
-        result = 1
+        result = 100000000
         for i in range(1, n + 2)         
         result *= i
         return result
